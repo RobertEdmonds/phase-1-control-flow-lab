cec5285a11252c8bdf9ff6216383cfbef473a404
Robert Edmonds <bobby.edmonds89@gmail.com>
2021-11-26 18:52:13 -0700

    commit tree 8f5d6c87f957fea2233b19a4036b61979478ba16

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance){
+  let response
+  if (distance <= 400){
+    response = "This one is on me!"
+  return response;}
+  else if (distance <= 2499 && distance >= 2000){
+    response = "I will gladly take your thirty bucks."
+  return response;}
+  else if (distance >= 2500){
+    response = 'No can do.'
+  return response;}
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  return city === "NYC"?"Ok, sounds good.":"No go."
+  // This is for if customer types NYC they will get a Ok, sounds good. Everything else they get a No go.
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(amount){
+  switch(amount){
+    case 'generous':
+      return 'Thank you so much.'
+    case 'not as generous':
+      return 'Thank you.'
+    default:
+      return 'Bye.'
+  }
   // Write your code here!
 }
